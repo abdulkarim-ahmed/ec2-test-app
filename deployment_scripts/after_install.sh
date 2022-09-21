3731b18b087c6398eebd160029aa952a6388e451
Abdulkarim Ahmed <abdulkarim.ahmed@screenlake.com>
2022-09-22 00:14:27 +0500

    script updated

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/ec2-test-app
+cd /home/ubuntu/ec2-test-app
 npm install
 npm install pm2 -g
--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-FILE=/home/ec2-user/ec2-test-app/index.js
+FILE=/home/ubuntu/ec2-test-app/index.js
 if test -f "$FILE"; then
    echo "$FILE exists"
-   cd /home/ec2-user/
+   cd /home/ubuntu/
    pm2 stop --silent index.js
    pm2 delete index.js
    killall -9 node
 else 
     echo "$FILE does not exist."
 fi
-cd /home/ec2-user/
+cd /home/ubuntu/
 sudo rm -rf ec2-test-app
 sudo mkdir ec2-test-app
 
