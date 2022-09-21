fccae6ccb5e8340a240bb29bef2bfade4912a22c
Abdulkarim Ahmed <abdulkarim.ahmed@screenlake.com>
2022-09-21 17:19:42 +0500

    added deployment scripts

---
+++ deployment_scripts/before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FILE=/home/ec2-user/ec2-test-app/index.js
+if test -f "$FILE"; then
+   echo "$FILE exists"
+   cd /home/ec2-user/
+   pm2 stop --silent index.js
+   pm2 delete index.js
+   killall -9 node
+else 
+    echo "$FILE does not exist."
+fi
+cd /home/ec2-user/
+sudo rm -rf app
+sudo mkdir app
+
